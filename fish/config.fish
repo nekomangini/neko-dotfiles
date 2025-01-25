be05f4b40499d3ab456145d7c5f002903eacf6c9
nekomangini <encar.luis.salazar@gmail.com>
2025-01-25 15:11:04 +0800

    update fish config
    cleanup

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,3 @@
-# greeting
-# function fish_greeting
-#     random choice "Hello!" Hi uwu Nekomangini
-# end
-
 ############################
 ####### alias ##############
 ############################
@@ -21,7 +16,6 @@ alias nk='NVIM_APPNAME="nekovim" nvim'
 alias av='NVIM_APPNAME="astronvim" nvim'
 
 # alias for neovide
-# alias nv='~/Downloads/neovide.AppImage'
 function nvide
     nohup ~/Downloads/neovide.AppImage >/dev/null 2>&1 & disown
 end
@@ -81,12 +75,11 @@ set -x PATH $PATH /opt/flutter/bin
 
 # ruby path
 set -gx GEM_HOME $HOME/.gem/ruby/3.3.0
-set -gx PATH $PATH $HOME/.gem/ruby/3.3.0/bin
-alias rails='ruby -r rubygems -e "require \"rails/cli\"; Rails::CLI.start"'
+fish_add_path $HOME/.gem/ruby/3.3.0/bin
 # alias for jekyll
 alias jekyll='jekyll.ruby3.3'
 # alias for ruby on rails
-alias rails='rails.ruby3.3'
+alias rails='bundle exec rails' # alias rails='rails.ruby3.3'
 
 # ghcup
 set -x PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH
@@ -106,22 +99,8 @@ end
 # export PATH=/home/nekomangini/.nimble/bin:$PATH
 set -gx PATH /home/nekomangini/.nimble/bin $PATH
 
-# Rust
-# function source_bash_env
-#     set -l env_file "$HOME/.cargo/env"
-#     if test -f $env_file
-#         # Source the env file in a bash sub-shell and export the variables to Fish
-#         for line in (bash -c "source $env_file && env | grep -E 'CARGO|RUST'")
-#             set -l key (echo $line | cut -d '=' -f 1)
-#             set -l value (echo $line | cut -d '=' -f 2-)
-#             set -gx $key $value
-#         end
-#     end
-# end
-
 # source_bash_env
 set -Ux fish_user_paths $HOME/.cargo/bin $fish_user_paths
-
 
 # zed text editor
 set -x PATH $HOME/.local/bin $PATH
@@ -146,61 +125,11 @@ set -gx PATH $HOME/.cabal/bin /home/nekomangini/.ghcup/bin $PATH # ghcup-env
 set -x PATH $HOME/.emacs.d/bin/ $PATH
 
 # ada: alire(ada package manager)
-# set -x PATH /home/nekomangini/ada_language_server/ $PATH
 set -x PATH /home/nekomangini/alr-2.0.2-bin-x86_64-linux/bin $PATH
 set -x PATH /home/nekomangini/.local/share/alire/toolchains/gprbuild_22.0.1_24dfc1b5/bin $PATH
 
 # fortls
 set -x PATH /home/nekomangini/.local/bin/fortls $PATH
-
-# skia
-# set -gx PATH (pwd)/depot_tools $PATH
-# set -gx PATH /home/nekomangini/deps/skia/third_party/externals/emsdk $PATH
-# set -gx PATH /home/nekomangini/deps/skia/third_party/externals/emsdk/node/14.18.2_64bit/bin $PATH
-# set -gx PATH /home/nekomangini/deps/skia/third_party/externals/emsdk/upstream/emscripten $PATH
-# set -x SKIA_DIR /run/media/nekomangini/D/skia/
-# set -x SKIA_LIBRARY_DIR /run/media/nekomangini/D/skia/out/Release/
-# set -x SKIA_LIBRARY skia
-# set -x SKIA_DIR /run/media/nekomangini/D/skia/
-# set -x SKIA_LIBRARY_DIR /run/media/nekomangini/D/skia/out/Release/
-# set -x SKIA_LIBRARY /run/media/nekomangini/D/skia/out/Release/libskia.a
-# set -x SKIA_LIBRARY /run/media/nekomangini/D/skia/out/Release/libskia.a
-# set -Ux fish_user_paths /run/media/nekomangini/D/skia/third_party/externals/emsdk $fish_user_paths
-# set -Ux fish_user_paths /run/media/nekomangini/D/skia/third_party/externals/emsdk/upstream/emscripten $fish_user_paths
-# cmake -G Ninja \
-#       -DLAF_BACKEND=skia \
-#       -DSKIA_DIR=/run/media/nekomangini/D/skia \
-#       -DSKIA_LIBRARY=/run/media/nekomangini/D/skia/out/Release/libskia.a \
-#       -DSKIA_LIBRARY_DIR=/run/media/nekomangini/D/skia/out/Release \
-#       ..
-# cmake -G Ninja \
-#       -DLAF_BACKEND=skia \
-#       -DSKIA_DIR=/run/media/nekomangini/D/skia \
-#       -DSKIA_LIBRARY=/run/media/nekomangini/D/skia/out/Release/libskia.a \
-#       -DSKIA_LIBRARY_DIR=/run/media/nekomangini/D/skia/out/Release \
-#       -DUSE_SHARED_FMT=OFF ..
-
-# There is a compatibility issues with sdkman in fish shell
-# THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-# set -x SDKMAN_DIR $HOME/.sdkman
-# if test -s "$HOME/.sdkman/bin/sdkman-init.fish"
-#     source $HOME/.sdkman/bin/sdkman-init.fish
-# end
-
-# using bass
-# set -x SDKMAN_DIR "$HOME/.sdkman"
-# test -s "$HOME/.sdkman/bin/sdkman-init.sh"; and bass source "$HOME/.sdkman/bin/sdkman-init.sh"
-#
-# set -x SDKMAN_DIR "$HOME/.sdkman"
-# if test -s "$HOME/.sdkman/bin/sdkman-init.sh"
-#     # Directly set PATH and other environment variables
-#     set -x PATH $HOME/.sdkman/candidates/*/current/bin $PATH
-#     
-#     # Load SDKMAN functions
-#     function sdk
-#         bash -c "source $HOME/.sdkman/bin/sdkman-init.sh && sdk $argv"
-#     end
-# end
 
 set -x SDKMAN_DIR "$HOME/.sdkman"
 if test -s "$HOME/.sdkman/bin/sdkman-init.sh"
