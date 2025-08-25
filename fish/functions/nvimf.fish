774bdf73f7107abe22d1936318521b8f91145caa
nekomangini <encar.luis.salazar@gmail.com>
2025-08-25 10:29:12 +0800

    fix(fish): use nvim instead of neovide

--- fish/functions/nvimf.fish
+++ fish/functions/nvimf.fish
@@ -1,7 +1,7 @@
-# astronvim neovide fzf
+# lazyvim fzf
 function nvimf
   set -l selected_file (fzf --preview "cat {}" --height 70%)
   if test -n "$selected_file"
-    neovide "$selected_file"
+    nvim "$selected_file"
   end
 end
