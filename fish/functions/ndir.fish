a875f367fabf3457082f69cee3467f609c5b4267
nekomangini <encar.luis.salazar@gmail.com>
2025-10-01 21:52:11 +0800

    cleanup directories

--- fish/functions/ndir.fish
+++ fish/functions/ndir.fish
@@ -1,35 +1,27 @@
 function ndir
   set -l DIRECTORIES \
     /run/media/nekomangini/D/Programming/android-projects/flutter-projects/                     \
-    /run/media/nekomangini/D/Programming/android-projects/flutter-projects/flutter-foundations/ \
     /run/media/nekomangini/D/Programming/neko-gitjournal/                                       \
-    /run/media/nekomangini/D/Programming/Projects/front-end-projects/                           \
-    /run/media/nekomangini/D/Programming/Projects/go-projects/                                  \
+    /run/media/nekomangini/D/Programming/Projects/                                              \
     /run/media/nekomangini/D/Programming/dotfiles/                                              \
     /run/media/nekomangini/D/Programming/neko-dotfiles/                                         \
+    /run/media/nekomangini/D/Programming/fedora-dotfiles/                                       \
+    /run/media/nekomangini/D/Programming/helix_config/                                          \
+    /run/media/nekomangini/D/Programming/kakoune_config/                                        \
     /run/media/nekomangini/D/Programming/blender-python/                                        \
     /run/media/nekomangini/D/Programming/scripts/                                               \
     /run/media/nekomangini/D/Programming/git-practice/                                          \
-    /run/media/nekomangini/D/Programming/programming-exercises/book/                            \
-    /run/media/nekomangini/D/Programming/programming-exercises/basics/                          \
-    /run/media/nekomangini/D/Programming/programming-exercises/basics-review                    \
-    /run/media/nekomangini/D/Programming/programming-exercises/youtube-tutorials/               \
-    /run/media/nekomangini/D/Programming/programming-exercises/frontend-development/            \
-    /run/media/nekomangini/D/game-development/save-files/Godot/godot_practice/                  \
-    /run/media/nekomangini/D/game-development/save-files/Godot/godot_yt_tutorials/              \
-    /run/media/nekomangini/D/game-development/save-files/Godot/godot-save-files/                \
-    /run/media/nekomangini/D/game-development/save-files/love2d/love2d-save-files/              \
-    /run/media/nekomangini/D/game-development/save-files/pygame/pygame-save-files/              \
-    /run/media/nekomangini/D/game-development/save-files/phaser/phaser-save-files/              \
-    /run/media/nekomangini/D/game-development/save-files/kaplay/kaplay-save-files/              \
-    /run/media/nekomangini/D/game-development/save-files/defold/defold-practice-files/          \
+    /run/media/nekomangini/D/emacs-save-files/emacs-org-sync/                                   \
+    /run/media/nekomangini/D/scratchpad-opensuse/                                               \
+    /run/media/nekomangini/D/Programming/obsidian-notes                                         \
+    /run/media/nekomangini/D/obsidian-files/nekomangini/                                        \
+    /run/media/nekomangini/D/Programming/programming-exercises/                                 \
+    /run/media/nekomangini/D/game-development/save-files/                                       \
     /home/nekomangini/.config/nekovim/                                                          \
     /home/nekomangini/.config/nvim/                                                             \
     /home/nekomangini/.config/astronvim_v5/                                                     \
     /home/nekomangini/.local/bin/bash-scripts/                                                  \
-    /run/media/nekomangini/D/scratchpad-opensuse/                                               \
-    /run/media/nekomangini/D/obsidian-files/nekomangini/                                        \
-
+    /home/nekomangini/rose-pine-temp
   set -l SELECTED_DIR (printf "%s\n" $DIRECTORIES | fzf --height 40% --reverse --prompt="Select a directory: ")
 
   if test -n "$SELECTED_DIR"
@@ -38,7 +30,7 @@ function ndir
     return 1
   end
   # echo "Navigated to $SELECTED_DIR"
-else
-  echo "No directory selected."
-end
+  else
+    echo "No directory selected."
+  end
 end
