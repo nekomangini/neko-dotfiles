bceb74a4c6dae19fcf88094dccda4e3cc9a714aa
nekomangini <encar.luis.salazar@gmail.com>
2025-10-21 20:38:07 +0800

    fix: Correct path to configuration.nix after refactor
    Updates the `flake.nix` file to correctly point the
    `nixosConfigurations.nixos` module to the new location:
    `./hosts/nixos/configuration.nix`, resolving the "path does not exist"
    error.

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
           modules = [
-            ./configuration.nix
+            ./hosts/nixos/configuration.nix
 
             # make home-manager as a module of nixos
             # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
