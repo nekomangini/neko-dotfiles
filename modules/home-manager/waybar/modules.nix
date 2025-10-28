186425eaa53313efd2f15c064b7d83831a6b7d48
nekomangini <encar.luis.salazar@gmail.com>
2025-10-28 16:50:04 +0800

    refactor: Modularize waybar configuration
    Restructures the Waybar setup by splitting the single `waybar.nix` file
    into dedicated modules.
    
    This improves maintainability by separating concerns:
    - `default.nix`: Main configuration entry point.
    - `style.nix`: Dedicated styling and appearance settings.
    - `modules.nix`: Defines the individual Waybar modules (e.g., clock,
    battery, workspace).

---
+++ modules/home-manager/waybar/modules.nix
@@ -0,0 +1,77 @@
+{ ... }:
+{
+  programs.waybar.settings.mainBar = {
+    "custom/launcher" = {
+      format = " ";
+      tooltip = false;
+    };
+
+    "hyprland/workspaces" = {
+      disable-scroll = true;
+      all-outputs = true;
+      tooltip = false;
+    };
+
+    tray = {
+      spacing = 10;
+      tooltip = false;
+    };
+
+    clock = {
+      format = "{:%I:%M - %a, %b %d %Y}";
+      tooltip = false;
+    };
+
+    temperature = {
+      thermal-zone = 2;
+      hwmon-path = "/sys/class/hwmon/hwmon2/temp1_input";
+      critical-threshold = 80;
+      format = "temp {temperatureC}°C";
+      format-critical = "temp {temperatureC}°C";
+      interval = 2;
+      states = {
+        critical = 80;
+      };
+    };
+
+    cpu = {
+      format = "cpu {usage}%";
+      interval = 2;
+      states = {
+        critical = 90;
+      };
+    };
+
+    memory = {
+      format = "mem {percentage}%";
+      interval = 2;
+      states = {
+        critical = 80;
+      };
+    };
+
+    network = {
+      format-wifi = "wifi {bandwidthDownBits}";
+      format-ethernet = "enth {bandwidthDownBits}";
+      format-disconnected = "no network";
+      interval = 5;
+      tooltip = false;
+    };
+
+    pulseaudio = {
+      scroll-step = 5;
+      max-volume = 150;
+      format = "vol {volume}%";
+      format-bluetooth = "vol {volume}%";
+      nospacing = 1;
+      on-click = "pwvucontrol";
+      tooltip = false;
+    };
+
+    "custom/control" = {
+      format = "⏻";
+      on-click = "hyprctl dispatch exit";
+      tooltip = false;
+    };
+  };
+}
