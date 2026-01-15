d01843679ef2e817e7521c189f27851bf5793a69
nekomangini <encar.luis.salazar@gmail.com>
2026-01-15 23:14:05 +0800

    refactor: Enable Tmux true color
    Improve terminal rendering.
    
    - **Tmux:** Enabled true color (24-bit) support to ensure themes like
    Everforest(Helix Editor) render accurately.

--- modules/home-manager/tmux.nix
+++ modules/home-manager/tmux.nix
@@ -13,8 +13,16 @@
       bind - split-window -v
 
       # Sessions
-      bind N new-session -c "#{pane_current_path}";
-      bind c new-window  -c "#{pane_current_path}";
-    '';
+      bind N new-session -c "#{pane_current_path}"
+      bind c new-window  -c "#{pane_current_path}"
+
+      # --- Color Fixes ---
+      # Enable true color support
+      set -g default-terminal "tmux-256color"
+
+      # Enable true color (24-bit) support
+      set -ga terminal-overrides ",*256col*:Tc"
+      set -ga terminal-overrides ",xterm-256color:Tc"
+      set -ga terminal-overrides ",xterm-kitty:Tc"    '';
   };
 }
